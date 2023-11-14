66637775f4433ecec20dd001c043ca2fa7e090d1
Alejandro Navarro <alejandropvp13@gmail.com>
2023-11-14 15:36:29 +0100

    Comienza THREEJS

--- cliente/juego.js
+++ cliente/juego.js
@@ -1,25 +1,46 @@
-//MIRAR COMO UTILIZAR EL THREE JS   
-import * as THREE from './three'
-//CREAR UNA SCENE CON THREEJS
+import * as THREE from "./node_modules/three/build/three.module.js";
+
+// Crear una escena con Three.js
 const scene = new THREE.Scene();
-//CREAR UNA CAMARA CON THREEJS
+
+// Crear una cámara con Three.js
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-//CREAR UN RENDER CON THREEJS
+
+// Crear un render con Three.js
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById("juego").appendChild(renderer.domElement);
-
-//CREAR UNA LUZ CON THREEJS
-const light = new THREE.HemisphereLight(0xFFFFFF, 1, 500);
-light.position.set(0, 0, 1);
-scene.add(light);
-//CREAR UNA FIGURA CON THREEJS
-//CREAR UNA TEXTURA CON THREEJS
-//CREAR UNA ANIMACION CON THREEJS
-//CREAR UNA INTERACCION CON THREEJS
-//CREAR UNA COLISION CON THREEJS
 
 const juego = document.getElementById("juego");
-const h1 = document.createElement("h1");
-h1.innerHTML = "Juego";
-juego.appendChild(h1);
+juego.appendChild(renderer.domElement);
+
+// Crear una luz hemisférica con Three.js y ajustar su color
+const light = new THREE.HemisphereLight(0x404040, 0x404040, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+// Establecer el color de fondo de la escena a negro
+scene.background = new THREE.Color(0x000000);
+
+// Crear un material con un color blanco frío para los objetos
+const material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF, flatShading: true });
+
+// Crear una geometría (por ejemplo, un cubo) con Three.js y aplicar el material
+const geometry = new THREE.BoxGeometry();
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+// Función de renderizado
+function animate() {
+    requestAnimationFrame(animate);
+
+    // Rotar el cubo para la animación
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+// Llamar a la función animate para iniciar la animación
+animate();
