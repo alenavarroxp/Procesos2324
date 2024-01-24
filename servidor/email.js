0adddd57a0e491dad204f3ace9e61802daeedfd9
Alejandro Navarro <alejandropvp13@gmail.com>
2024-01-24 17:10:13 +0100

    Update greeting message in email template

--- servidor/email.js
+++ servidor/email.js
@@ -81,7 +81,7 @@ module.exports.enviarEmail = async function (direccion, key, men) {
               <h1>Bienvenido al Sistema de Partidos de Fútbol</h1>
           </div>
           <div class="content">
-              <p>Hola,</p>
+              <p>Hola!</p>
               <p>Gracias por unirte a nuestro sistema. Para confirmar tu cuenta y empezar a disfrutar de los partidos de fútbol, por favor haz clic en el botón a continuación:</p>
               <a class="button" href="${url}confirmarUsuario/${direccion}/${key}">Confirmar Cuenta</a>
               <p>¡Nos vemos en el campo!</p>
